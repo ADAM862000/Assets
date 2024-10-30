a4070c7139e32b89787d46790d745df54cb8f1e4
Adam <adam862000.b@gmail.com>
2024-10-30 17:28:39 +0300

    Assignment_8

--- Scripts/Assignment_8.cs
+++ Scripts/Assignment_8.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class script : MonoBehaviour
+public class Assignment_8 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
