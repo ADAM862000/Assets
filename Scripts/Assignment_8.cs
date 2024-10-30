ac6d3aa09f1e330327ff5614a7274661a9b0c539
Adam <adam862000.b@gmail.com>
2024-10-30 17:25:48 +0300

    first commit

---
+++ Scripts/Assignment_8.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        System.Random random = new System.Random();
+        
+        while (true)
+        {
+            int number = random.Next(1, 21);
+
+            if (number == 5)
+            {
+                continue;
+            }
+
+            Debug.Log("Generated number: " + number);
+            
+            if (number == 15)
+            {
+                break;
+            }
+        }
+
+    }
+}
